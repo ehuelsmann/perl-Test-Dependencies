f830a420b459aea5ddc556c144f40bfba8faad8a
Erik Huelsmann <ehuels@gmail.com>
2020-10-10 22:25:57 +0200

    Release 0.29

--- lib/Test/Dependencies.pm
+++ lib/Test/Dependencies.pm
@@ -15,11 +15,11 @@ Test::Dependencies - Ensure that the dependency listing is complete
 
 =head1 VERSION
 
-Version 0.28
+Version 0.29
 
 =cut
 
-our $VERSION = '0.28';
+our $VERSION = '0.29';
 
 =head1 SYNOPSIS
 
