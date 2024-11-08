652733e3d66b786d02eee6559fb7efb4a8af04e0
Erik Huelsmann <ehuels@gmail.com>
2024-11-08 23:50:19 +0100

    Release 0.33

--- lib/Test/Dependencies.pm
+++ lib/Test/Dependencies.pm
@@ -15,11 +15,11 @@ Test::Dependencies - Ensure that the dependency listing is complete
 
 =head1 VERSION
 
-Version 0.32
+Version 0.33
 
 =cut
 
-our $VERSION = '0.32';
+our $VERSION = '0.33';
 
 =head1 SYNOPSIS
 
